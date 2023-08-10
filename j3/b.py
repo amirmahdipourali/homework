a1b92845d39306be199bf813e285340a3e1a8a9e
amirmahdi pourali <140058795+amirmahdipourali@users.noreply.github.com>
2023-08-10 10:58:49 +0330

    Delete j3 directory

--- j3/b.py
+++
@@ -1,3 +0,0 @@
-celsius=int(input('enter celsius: '))
-f=int(celsius*2+30)
-print(f)
--- j3/c.py
+++
@@ -1,6 +0,0 @@
-a=(input("plaese enter first number: "))
-b=(input("plaese enter secend number: "))
-if (a>b) 
-print(a)
-else
-print (b)
