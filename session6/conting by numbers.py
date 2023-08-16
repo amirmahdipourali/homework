54e6225e0cbd339e08a06f41c98e0f08967b45eb
amirmahdi pourali <140058795+amirmahdipourali@users.noreply.github.com>
2023-08-16 21:49:40 +0330

    Delete session6 directory

--- session6/conting by numbers.py
+++
@@ -1,5 +0,0 @@
-number=int(input("enter your number: "))
-conter= 1
-while conter <= number:
-    print(conter,"'"*conter,sep='',end='')
-    conter+= 1
--- session6/spece.py
+++
@@ -1,4 +0,0 @@
-a=(input("enter what you want to say: "))
-while "" in a :
-    a.replace("","d")
-print(a)
