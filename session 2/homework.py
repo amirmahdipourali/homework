45d6d3de9c97349d1fcda1905db6bfa1137ed7f4
amirmahdi pourali <140058795+amirmahdipourali@users.noreply.github.com>
2023-08-16 21:48:19 +0330

    Delete session 2 directory

--- session 2/homework.py
+++
@@ -1,5 +0,0 @@
-print('welcom to my sacend homework assignment ')
-number1=int(input('enter number1:  '))
-number2=int(input('enter number2:  '))
-total = number1 + number2
-print(number1," + ",number2," = ",total)
