ece5fd551ab0e4f8e9fa411752cac663def1cef2
amirmahdi pourali <140058795+amirmahdipourali@users.noreply.github.com>
2023-09-05 23:14:09 +0330

    Add files via upload

---
+++ session8/data structures.py
@@ -0,0 +1,17 @@
+list= []
+set = {}
+dict = {}
+tuple = ()
+#set
+colors={'blue','black','lime'}
+print(colors)
+#list
+car_brand=["bmw","lb","tt",'ll']
+print(car_brand)
+#dict
+dict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+tuple=("a","b","c","d")
